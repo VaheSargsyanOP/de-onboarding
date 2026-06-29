f36f51516a9b576835ab4773b16d1f983440019b
VaheSargsyanOP <Vahe.Sargsyan@optimumpartners.com>
2026-06-29 13:30:15 +0400

    airflow: added step 2: several run types

---
+++ dags/weather_pipeline.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+
+with DAG(
+    dag_id="weather_pipeline",
+    description="Weather ingestion pipeline",
+    start_date=datetime(2026, 1, 1),
+    schedule="0 6 * * *",
+    catchup=False,
+    tags=["weather"],
+) as dag:
+
+    download_weather = BashOperator(
+        task_id="download_weather",
+        bash_command="""
+        cd /opt/airflow/project
+
+        if [ -n "{{ dag_run.conf.get('date_from', '') }}" ] && \
+        [ -n "{{ dag_run.conf.get('date_to', '') }}" ]; then
+
+            python download_weather.py \
+                --city Yerevan \
+                --date_from {{ dag_run.conf.get('date_from') }} \
+                --date_to {{ dag_run.conf.get('date_to') }}
+
+        elif [ -n "{{ dag_run.conf.get('date', '') }}" ]; then
+
+            python download_weather.py \
+                --city Yerevan \
+                --date {{ dag_run.conf.get('date') }}
+
+        else
+
+            python download_weather.py \
+                --city Yerevan
+
+        fi
+        """
+)
